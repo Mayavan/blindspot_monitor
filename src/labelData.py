413239e02b27dfeafa509c14075c19c97eb525d2
rajendransathyamm <Mayavan.RajendranSathyam@ext.panasonicautomotive.com>
2021-03-31 23:31:35 -0400

    Update label data to use jupyter notebook

--- src/labelData.py
+++ src/labelData.py
@@ -1,29 +1,17 @@
+# %%
 import cv2
 import argparse
 import os
+from pathlib import Path
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", help="Path of Video file to label")
-parser.add_argument("--out", help="Path of data directory")
-parser.add_argument("--frames_from_folder", help="Use to use frames already in out directory", action="store_true")
-args = parser.parse_args()
+# %%
+import json
+with open("../label_data_config.json") as json_file:
+            config = json.load(json_file)
 
-if(not args.frames_from_folder):
-    if not args.file:
-        file_path = '../../day_right_1.mp4'
-    else:
-        file_path = args.file
-
-
-if not args.out:
-    data_dir = '../data'
-else:
-    data_dir = args.out
-
-if args.frames_from_folder:
-    prepare_frames = False
-else:
-    prepare_frames = True
+file_path = config["file_path"]
+data_dir = config["data_dir"]
+prepare_frames = config["prepare_frames"]
 
 
 try:
@@ -36,20 +24,21 @@ except OSError as e:
 
 if(prepare_frames):
     vidcap = cv2.VideoCapture(file_path)
-    success,image = vidcap.read()
+    success, image = vidcap.read()
     count = 0
     print("Preparing Frames")
     while success:
-        cv2.imwrite("{}/{}.jpg".format(data_dir,count), image)     # save frame as JPEG file
-        success,image = vidcap.read()
+        cv2.imwrite("{}/{}.jpg".format(data_dir, count),
+                    image)     # save frame as JPEG file
+        success, image = vidcap.read()
         count += 1
-
+# %%
 count = 0
 skip = 0
 label = {}
 
 while True:
-    image  = cv2.imread("{}/{}.jpg".format(data_dir,count))
+    image = cv2.imread("{}/{}.jpg".format(data_dir, count))
     if(image is None):
         break
 
@@ -58,16 +47,22 @@ while True:
     key = cv2.waitKey(0)
     if key == ord('5'):
         # Mark 1 frame as false
+        label["{}.jpg".format(count)] = 0
         count += 1
     elif key == ord('6'):
         # Mark 30 frames as false
         for i in range(30):
             label["{}.jpg".format(count)] = 0
             count += 1
-    elif key == ord('0'):
+    elif key == ord('2'):
         # Mark current frame as true
         label["{}.jpg".format(count)] = 1
         count += 1
+    elif key == ord('1'):
+        # Mark 30 frames as true
+        for i in range(30):
+            label["{}.jpg".format(count)] = 1
+            count += 1
     elif key == ord('7'):
         # Go back 30 frames
         count -= 30
@@ -80,8 +75,43 @@ while True:
     elif key == ord('d'):
         pass
 
-f = open(data_dir+"/label.txt", "w")
-for key, val in label.items():
-    f.write("{} {}\n".format(key, val))
+# %%
+print("Arranging Data")
 
-f.close()
+percent_train = 60
+percent_test = 20
+percent_val = 20
+
+count = 0
+
+for fileName, cur_label in label.items():
+    # Find sub directory
+    if(count % 100 < percent_train):
+        sub_dir = "train"
+    elif(count % 100 < percent_test+percent_train):
+        sub_dir = "test"
+    else:
+        sub_dir = "val"
+
+    # Find class
+    if(cur_label == 1):
+        # Occupied class
+        class_name = "occupied"
+    else:
+        # Free class
+        class_name = "free"
+
+    if not os.path.exists("{}/{}/{}/".format(data_dir, sub_dir, class_name)):
+        os.makedirs("{}/{}/{}/".format(data_dir, sub_dir, class_name))
+
+    if(Path("{}/{}".format(data_dir, fileName)).is_file()):
+        Path("{}/{}".format(data_dir, fileName)
+             ).rename("{}/{}/{}/{}".format(data_dir, sub_dir, class_name, fileName))
+    else:
+        print("{}/{}".format(data_dir, fileName) + " does not exist")
+
+    count += 1
+
+
+
+# %%
